0162e479a40001d438908bec8bfdb7537f0e05c7
Ibrahim <ibrahim.tigrek@gmail.com>
2018-12-10 00:22:55 +0300

    uploaded infixexpression java file

---
+++ InfixExpression.java
@@ -0,0 +1,31 @@
+package pkg;
+
+import java.util.Stack;
+
+public class InfixExpression {
+	public static void printInfix(String input) {
+		String[] values = input.split("\\s");
+		Stack<String> operators = new Stack<String>();
+		Stack<String> operands = new Stack<String>();
+		for (int i = 0; i < values.length; i++) {
+			String value = values[i];
+			if (value.equals(")")) {
+				String b = operands.pop();
+				String a = operands.pop();
+				String op = operators.pop();
+				operands.push("( " + a + " " + op + " " + b + " )");
+			} else if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
+				operators.push(value);
+			} else {
+				// value is an integer
+				operands.push(value);
+			}
+		}
+		System.out.println(operands.pop());
+	}
+	
+	public static void main(String[] args) {
+		printInfix("1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )");
+	}
+	
+}
