c76978bc14a934043f162e1cea90cd913fa54525
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2018-12-24 01:25:36 +0300

    Update CombineArrays.java

--- CombineArrays.java
+++ CombineArrays.java
@@ -5,36 +5,27 @@
 package pkg;
 
 public class CombineArrays {
-	public static int[] combine (int[] a, int[] b) {
-		int[] combo = new int[a.length];
-		int comboInd = 0;
-		int ai = 0; // index of array a
-		int bi = 0; // index of array b
+	public static void combine (int[] a, int[] b) {
+		int ai = 0;
+		int bi = 0;
 		while (true) {
 			if (ai >= a.length || bi >= b.length)
-				break;  // out of bounds
-			if (a[ai] < b[bi])  // if current value from array a is smaller, move to next value
+				break;
+			if (a[ai] < b[bi])
 				ai++;
-			else if (a[ai] > b[bi])  // if current value from array b is smaller, move to next value
+			else if (a[ai] > b[bi])
 				bi++;
 			else {
-				// if the two values are equal, we have found a matching value
-				combo[comboInd++] = a[ai];  // add to our array
+				System.out.printf("%d ", a[ai]);
 				ai++;
 				bi++;
 			}
 		}
-		int[] finalArr = new int[comboInd];
-		for (int i = 0; i < comboInd; i++)  // get rid of extra spaces in array
-			finalArr[i] = combo[i];
-		return finalArr;
 	}
 	
 	public static void main (String[] args) {
 		int[] a = new int[] {1, 2, 2, 3, 17, 29};
 		int[] b = new int[] {2, 2, 29, 219};
-		int[] c = combine(a, b);
-		for (int i = 0; i < c.length; i++)
-			System.out.printf("%d ", c[i]);
+		combine(a, b);
 	}
 }
