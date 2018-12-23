1580a2f7a9c528c7ab59f9e60cc1aff81f24dee1
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2018-12-24 00:24:23 +0300

    Add files via upload

---
+++ exercise1_3_21.java
@@ -0,0 +1,10 @@
+﻿//	1.3.21 Write a method find() that takes a linked list and a string key as arguments
+//	and returns true if some node in the list has key as its item ﬁeld, false otherwise.
+
+public boolean find(Node node, String key) {
+	while (true) {
+		if (node.item.equals(key)) return true;
+		if (node.hasNext()) node = node.next;
+		else return false;
+	}
+}
