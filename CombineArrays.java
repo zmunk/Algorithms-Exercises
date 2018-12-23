cefa3c4a68686ad5d1c0b8bfc21ebfd079da4ca8
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2018-12-24 01:28:38 +0300

    Update CombineArrays.java

--- CombineArrays.java
+++ CombineArrays.java
@@ -6,16 +6,17 @@ package pkg;
 
 public class CombineArrays {
 	public static void combine (int[] a, int[] b) {
-		int ai = 0;
-		int bi = 0;
+		int ai = 0; // index of array a
+		int bi = 0; // index of array b
 		while (true) {
-			if (ai >= a.length || bi >= b.length)
+			if (ai >= a.length || bi >= b.length) // out of bounds
 				break;
-			if (a[ai] < b[bi])
+			if (a[ai] < b[bi]) // if current value from array a is smaller, move to next value
 				ai++;
-			else if (a[ai] > b[bi])
+			else if (a[ai] > b[bi]) // if current value from array b is smaller, move to next value
 				bi++;
 			else {
+				// if the two values are equal, we have found a matching value
 				System.out.printf("%d ", a[ai]);
 				ai++;
 				bi++;
