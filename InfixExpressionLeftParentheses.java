0cbf77516c727bdfb2a570972d72a93295f0dd7d
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2019-01-09 21:50:13 +0300

    Update InfixExpressionLeftParentheses.java

--- InfixExpressionLeftParentheses.java
+++ InfixExpressionLeftParentheses.java
@@ -1,32 +1,29 @@
 // Write a program that takes an expression without right parentheses
 //	 and prints the equivalent infix expression with the parentheses inserted.
-//   e.g. " ( ( ( 1 + 2  * ( 3 - 4  * ( 5 - 6 "
+//   e.g. " ( ( ( 1 + 2 * ( 3 - 4 * ( 5 - 6 "
 
 package pkg;
 
 import java.util.Stack;
 
 public class InfixExpressionLeftParentheses {
-	public static void printInfix(String input) {
-		String[] values = input.split("\\s");
-		Stack<String> stack = new Stack<String>();
-		for (int i = values.length - 1; i >= 0; i--) {
-			String value = values[i];
-			if (value.equals("(")) {
-				String a = stack.pop();
-        String op = stack.pop();
-				String b = stack.pop();
-				stack.push("( " + a + " " + op + " " + b + " )");
-			} else {
-				// value is an integer or an operator
-				stack.push(value);
-			}
-		}
-		System.out.println(stack.pop());
+    public static void printInfix(String input) {
+        Stack<String> stack = new Stack<String>();
+		for (int i = input.length() - 1; i >= 0; i--) {
+            String value = input.substring(i, i + 1);
+            if (value.equals(" "))
+                continue;
+          	System.out.println(value);
+            if (value.equals("("))
+                stack.push("( " + stack.pop() + " " + stack.pop() + " " + stack.pop() + " )");
+            else
+                stack.push(value);
+        }
+      	System.out.println(stack.pop());
 	}
 	
 	public static void main(String[] args) {
-		printInfix(" ( ( ( 1 + 2  * ( 3 - 4  * ( 5 - 6 ");
+		printInfix(" ( ( ( 1 + 2 * ( 3 - 4 * ( 5 - 6 ");
 	}
 	
 }
