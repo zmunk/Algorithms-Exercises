94144511a78aa24a018e863d2ce4ae2c791cb465
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2019-01-09 21:55:46 +0300

    Update InfixExpressionLeftParentheses.java

--- InfixExpressionLeftParentheses.java
+++ InfixExpressionLeftParentheses.java
@@ -21,8 +21,7 @@ public class InfixExpressionLeftParentheses {
         }
         System.out.println(stack.pop());
     }
-	
-	public static void main(String[] args) {
-		printInfix(" ( ( ( 1 + 2 * ( 3 - 4 * ( 5 - 6 ");
-	}
+    static void main(String[] args) {
+        printInfix(" ( ( ( 1 + 2 * ( 3 - 4 * ( 5 - 6 ");
+    }
 }
