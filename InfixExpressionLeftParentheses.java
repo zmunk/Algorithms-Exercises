a58797b8920d3e5f29f1fe6b83461e71fd4cbbc0
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2019-01-09 21:54:31 +0300

    Update InfixExpressionLeftParentheses.java

--- InfixExpressionLeftParentheses.java
+++ InfixExpressionLeftParentheses.java
@@ -9,17 +9,17 @@ import java.util.Stack;
 public class InfixExpressionLeftParentheses {
     public static void printInfix(String input) {
         Stack<String> stack = new Stack<String>();
-	    for (int i = input.length() - 1; i >= 0; i--) {
+        for (int i = input.length() - 1; i >= 0; i--) {
             String value = input.substring(i, i + 1);
             if (value.equals(" "))
                 continue;
-          	System.out.println(value);
+            System.out.println(value);
             if (value.equals("("))
                 stack.push("( " + stack.pop() + " " + stack.pop() + " " + stack.pop() + " )");
             else
                 stack.push(value);
         }
-      	    System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 	
 	public static void main(String[] args) {
